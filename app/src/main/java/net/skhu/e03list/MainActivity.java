d8117253c18428a4cd7fe4f0f162f31272423036
ccomapjw <ccomapjw@naver.com>
2020-04-30 15:44:57 +0900

    pjw

--- app/src/main/java/net/skhu/e03list/MainActivity.java
+++ app/src/main/java/net/skhu/e03list/MainActivity.java
@@ -24,4 +24,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void btnRecyclerView2_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView2Activity.class);
+        startActivity(intent);
+    }
+
+    public void btnRecyclerView3_clicked(View view) {
+        Intent intent = new Intent(this, RecyclerView3Activity.class);
+        startActivity(intent);
+    }
+
 }
